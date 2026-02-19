645273b682bd4ecf084ffc1ba94a5c93b96c78c2
Slava Muravev <SlamurMunch@gmail.com>
2026-02-19 15:34:49 +0600

    Sensors example: additional dag that blocked by sensors

---
+++ dags/no_schedule_dag.py
@@ -0,0 +1,18 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    schedule=None,
+    start_date=datetime(2026, 2, 19),
+    tags=['sensor'],
+    catchup=False
+)
+def no_schedule_dag():
+
+    @task
+    def runme():
+        print("Hi")
+
+    runme()
+
+no_schedule_dag()
