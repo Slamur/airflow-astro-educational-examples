572bd300398c758787797e5f25e3a48ff8b7b1ba
Slava Muravev <SlamurMunch@gmail.com>
2026-02-19 15:07:46 +0600

    Academy course: added bash and xcom dags examples

---
+++ dags/check_dag.py
@@ -0,0 +1,31 @@
+from airflow.sdk import dag, task
+from pendulum import datetime
+
+
+@dag(
+    start_date=datetime(2026, 2, 18),
+    schedule="@daily",
+    description="DAG to check file creation process",
+    tags=["example", "bash"],
+)
+def check_dag():
+
+    tmp_file_path = "/tmp/test_file.txt"
+
+    @task.bash
+    def create_file() -> None:
+        return f"echo 'This is a test file.' > {tmp_file_path}"
+
+    @task.bash
+    def check_file_exists() -> None:
+        return f"test -f {tmp_file_path}"
+
+    @task
+    def read_file() -> str:
+        with open(tmp_file_path, "r") as file:
+            content = file.read()
+        return content
+
+    create_file() >> check_file_exists() >> read_file()
+
+check_dag()
---
+++ dags/xcom_dag.py
@@ -0,0 +1,75 @@
+from airflow.sdk import dag, task, Context
+from pendulum import datetime
+
+
+@dag(
+    start_date=datetime(2026, 2, 19),
+    schedule="@daily",
+    description="DAG with XComs",
+    tags=["example", "xcom"],
+)
+def xcom_dag():
+
+    @task
+    def push_in_context(**context: Context) -> None:
+        value = "Hello from XCom!"
+        context["ti"].xcom_push(key="xcom_key", value=value)
+
+    @task
+    def get_from_context(**context: Context) -> None:
+        value = context["ti"].xcom_pull(key="xcom_key", task_ids="push_in_xcom")
+        print(f"Value retrieved from XCom: '{value}'")
+
+    @task
+    def second_push_same_key(ti) -> None:
+        value = "Hello again from XCom!"
+        ti.xcom_push(key="xcom_key", value=value)
+
+    @task
+    def extract_multiple_values_same_key(ti) -> None:
+        values = ti.xcom_pull(key="xcom_key", task_ids=["push_in_context", "second_push_same_key"])
+        print(f"Values retrieved from multiple tasks: {values}")
+
+    @task
+    def push_dict(ti) -> None:
+        value = {
+            "message": "Hello from a dictionary in XCom!",
+            "number": 42,
+        }
+        ti.xcom_push(key="dict_key", value=value)
+
+    @task
+    def get_dict(ti) -> None:
+        value = ti.xcom_pull(key="dict_key", task_ids="push_dict")
+        print(f"Dictionary retrieved from XCom: {value}")
+    
+    @task
+    def push_in_ti(ti) -> None:
+        value = "Hello from Task Instance!"
+        ti.xcom_push(key="ti_key", value=value)
+
+    @task
+    def get_from_ti(ti) -> None:
+        value = ti.xcom_pull(key="ti_key", task_ids="push_in_ti")
+        print(f"Value retrieved from Task Instance XCom: '{value}'")
+
+    @task
+    def return_value() -> str:
+        return "Hello from return value!"
+    
+    @task
+    def use_return_value(value: str) -> None:
+        print(f"Value retrieved from return value: '{value}'")
+
+    first_push = push_in_context()
+    first_push >> get_from_context()
+
+    first_push >> second_push_same_key() >> extract_multiple_values_same_key()
+
+    push_in_ti() >> get_from_ti()
+    push_dict() >> get_dict()
+
+    value = return_value()
+    use_return_value(value=value)
+
+xcom_dag()
