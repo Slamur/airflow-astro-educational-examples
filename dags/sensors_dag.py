f365ea2f80d6fbd9c91946d04681c60f456ef671
Slava Muravev <SlamurMunch@gmail.com>
2026-02-19 15:25:28 +0600

    Added sensors example: wait and use all task slots

---
+++ dags/sensors_dag.py
@@ -0,0 +1,33 @@
+from airflow.decorators import dag, task
+from airflow.sensors.filesystem import FileSensor
+from pendulum import datetime
+
+@dag(
+    schedule=None,
+    start_date=datetime(2026, 2, 19),
+    tags=['sensor'],
+    catchup=False
+)
+def sensors_dag():
+
+    files_cnt = 3
+
+    filepaths = [
+        f'data_{i}.csv' 
+        for i in range(1, 1 + files_cnt)
+    ]
+
+    wait_for_files = FileSensor.partial(
+        task_id='wait_for_files',
+        fs_conn_id='fs_default',
+    ).expand(
+        filepath=filepaths,
+    )
+
+    @task
+    def process_file():
+        print("I processed the file!")
+
+    wait_for_files >> process_file()
+
+sensors_dag()
