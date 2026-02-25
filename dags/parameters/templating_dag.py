581849d0852e5ea5eeef5d4334a102fb5b494e1a
Slava Muravev <SlamurMunch@gmail.com>
2026-02-25 21:21:39 +0600

    Templating lesson: getting ids to sep method

--- dags/parameters/templating_dag.py
+++ dags/parameters/templating_dag.py
@@ -9,22 +9,21 @@ from pendulum import datetime
 )
 def templating_dag():
 
+    def _get_ids_from_context():
+        context = get_current_context()
+        return context['dag_run'].conf.get('ids', [])
+
     @task
     def python_task():
-        context = get_current_context()
-
-        ids = context['dag_run'].conf.get('ids', [])
-
+        ids = _get_ids_from_context()
         ids_sum = sum(ids, 0)
         print(f"The sum of the ids is: {ids_sum}")
 
     @task.bash
     def bash_task():
-        context = get_current_context()
-        ids = context['dag_run'].conf.get('ids', [])
+        ids = _get_ids_from_context()
         ids_str = ",".join(map(str, ids))
         return f"bash $AIRFLOW_HOME/include/templating/scripts/script.sh {ids_str}"
-
 
     python_task()
     bash_task()
